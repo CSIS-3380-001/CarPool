549e718e15e9263b07a97cdde76110f1029b8609
Gaurav Mehla <gaurav@mehla.in>
2023-07-05 20:46:00 +0000

    Sidebar vertical height adjusted

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
-import './Sidebar.css';
+import React from 'react';
 
 export const Sidebar = () => {
   return (
-    <div>
-        Ninja
-    </div>
+        <div className="col-2 bg-dark sidebar" style={{ height: '100vh' }}>
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <a className="nav-link text-light" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-light" href="/rental-form">
+                Post
+              </a>
+            </li>
+          </ul>
+        </div>
   );
 };
