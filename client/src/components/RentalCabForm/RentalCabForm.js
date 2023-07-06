6cb707066387221edb56c6bb54e522133fa8c2cc
Anshdeep Singh <69358318+Anshdeep-Singh@users.noreply.github.com>
2023-07-06 15:52:27 -0700

    bootstrap theme implemented

--- client/src/components/RentalCabForm/RentalCabForm.js
+++ client/src/components/RentalCabForm/RentalCabForm.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./RentalCabForm.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Table from "react-bootstrap/Table";
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 
 export const RentalCabForm = () => {
@@ -10,18 +14,16 @@ export const RentalCabForm = () => {
   const [vehicle, setVehicle] = useState({
     name: "Toyota Camry",
     make: "Toyota",
-    plateNumber: "Camry",
+    plateNumber: "",
   });
 
   const [availability, setAvailability] = useState({
+    from_location: "New Westminster",
     time: "08:00",
     days: "2",
+    pickup: new Date(),
   });
 
-  const [locations, setLocations] = useState({
-    pickup: null,
-    dropOff: null,
-  });
 
   const [costs, setCosts] = useState({
     fare: "150",
@@ -45,19 +47,6 @@ export const RentalCabForm = () => {
     });
   };
 
-  const handlePickupChange = (date) => {
-    setLocations({
-      ...locations,
-      pickup: date,
-    });
-  };
-
-  const handleDropOffChange = (date) => {
-    setLocations({
-      ...locations,
-      dropOff: date,
-    });
-  };
 
   const handleCostsChange = (e) => {
     setCosts({
@@ -104,34 +93,42 @@ export const RentalCabForm = () => {
               />
             </label>
             <label>
-              License Plate Number:
+              Plate Number:
               <input
                 type="text"
-                name="model"
+                name="plateNumber"
                 value={vehicle.plateNumber}
                 onChange={handleVehicleChange}
               />
             </label>
-            <button onClick={handleNext} className="next-button">Next</button>
+
+            <Button
+              variant="success"
+              onClick={handleNext}
+              className="next-button"
+            >
+              Next
+            </Button>{" "}
           </div>
         );
       case 2:
         return (
           <div>
             <h2>Availability</h2>
-
             <label>
               Pickup On:
               <DatePicker
-                selected={locations.pickup}
-                onChange={handlePickupChange}
+                selected={availability.pickup}
+                onChange={handleAvailabilityChange}
                 dateFormat="MMMM d, yyyy"
                 placeholderText="Select a pickup date"
+                value={availability.pickup}
               />
             </label>
             <label>
               From Time:
-              <select
+              <Form.Select
+                aria-label="Default select example"
                 name="time"
                 value={availability.time}
                 onChange={handleAvailabilityChange}
@@ -161,12 +158,12 @@ export const RentalCabForm = () => {
                 <option value="21:00">9:00 PM</option>
                 <option value="22:00">10:00 PM</option>
                 <option value="23:00">11:00 PM</option>
-              </select>
+              </Form.Select>
             </label>
-
             <label>
               Number of Days Available for Renting:
-              <select
+              <Form.Select
+                aria-label="Default select example"
                 name="days"
                 value={availability.days}
                 onChange={handleAvailabilityChange}
@@ -177,10 +174,22 @@ export const RentalCabForm = () => {
                 <option value="3">3 days</option>
                 <option value="4">4 days</option>
                 <option value="5">5 days</option>
-              </select>
+              </Form.Select>
             </label>
-            <button onClick={handleNext} className="next-button" >Next</button>
-            <button onClick={handlePrev} className="previous-button"  >Previous</button>
+            <Button
+              variant="success"
+              onClick={handleNext}
+              className="next-button"
+            >
+              Next
+            </Button>{" "}
+            <Button
+              variant="secondary"
+              onClick={handlePrev}
+              className="previous-button"
+            >
+              Previous
+            </Button>{" "}
           </div>
         );
       case 3:
@@ -196,90 +205,118 @@ export const RentalCabForm = () => {
                 onChange={handleImageUpload}
               />
             </label>
-            <button onClick={handleNext} className="next-button" >Next</button>
-            <button onClick={handlePrev} className="previous-button"  >Previous</button>
+            <Button
+              variant="success"
+              onClick={handleNext}
+              className="next-button"
+            >
+              Next
+            </Button>{" "}
+            <Button
+              variant="secondary"
+              onClick={handlePrev}
+              className="previous-button"
+            >
+              Previous
+            </Button>{" "}
           </div>
         );
       case 4:
         return (
           <div>
             <form onSubmit={handleSubmit}>
-            <h2>Pricing</h2>
-            <label>
-              Daily Rate:
-              <input
-                type="text"
-                name="fare"
-                value={costs.fare}
-                onChange={handleCostsChange}
-              />
-            </label>
-            <label>
-              Deposit Amount:
-              <input
-                type="text"
-                name="deposit"
-                value={costs.deposit}
-                onChange={handleCostsChange}
-              />
-            </label>
-            <label>
-              Late Return Fee:
-              <input
-                type="text"
-                name="otherCosts"
-                value={costs.otherCosts}
-                onChange={handleCostsChange}
-              />
-            </label>
-            <button onClick={handleSubmit} className="next-button" >Submit</button>
-            <button onClick={handlePrev} className="previous-button" >Previous</button>
-            <div class="form-summary">
-  <h2>Summary</h2>
-  <table>
-    <tr>
-      <th>Vehicle:</th>
-      <td>{vehicle.name}</td>
-    </tr>
-    <tr>
-      <th>Make:</th>
-      <td>{vehicle.make}</td>
-    </tr>
-    <tr>
-      <th>Plate Number:</th>
-      <td>{vehicle.plateNumber}</td>
-    </tr>
-    <tr>
-      <th>Availability:</th>
-      <td>From - {availability.time} For - {availability.days} Days</td>
-    </tr>
-    <tr>
-      <th>Deposit:</th>
-      <td>{costs.deposit}$</td>
-    </tr>
-    <tr>
-      <th>Fare Per Day:</th>
-      <td>{costs.fare}$</td>
-    </tr>
-    <tr>
-      <th>Late Return Fee:</th>
-      <td>{costs.otherCosts}$</td>
-    </tr>
-    <tr>
-      <th>Images Uploaded:</th>
-      <td>{images.length}</td>
-    </tr>
-  </table>
-  <div class="image-preview">
-    {images.map((image, index) => (
-    <div class="image-preview-item" key={index}>
-      <img src={URL.createObjectURL(image)} alt={`Preview ${index}`} />
-    </div>
-    ))}
-  </div>
-</div>
-
-              </form>
+              <h2>Pricing</h2>
+              <label>
+                Daily Rate:
+                <input
+                  type="text"
+                  name="fare"
+                  value={costs.fare}
+                  onChange={handleCostsChange}
+                />
+              </label>
+              <label>
+                Deposit Amount:
+                <input
+                  type="text"
+                  name="deposit"
+                  value={costs.deposit}
+                  onChange={handleCostsChange}
+                />
+              </label>
+              <label>
+                Late Return Fee:
+                <input
+                  type="text"
+                  name="otherCosts"
+                  value={costs.otherCosts}
+                  onChange={handleCostsChange}
+                />
+              </label>
+              <Button
+                variant="success"
+                onClick={handleSubmit}
+                className="next-button"
+              >
+                Submit
+              </Button>{" "}
+              <Button
+                variant="secondary"
+                onClick={handlePrev}
+                className="previous-button"
+              >
+                Previous
+              </Button>{" "}
+              <div className="form-summary">
+                <h2>Summary</h2>
+                <Table striped bordered hover>
+                  <tr>
+                    <th>Vehicle:</th>
+                    <td>{vehicle.name}</td>
+                  </tr>
+                  <tr>
+                    <th>Make:</th>
+                    <td>{vehicle.make}</td>
+                  </tr>
+                  <tr>
+                    <th>Plate Number:</th>
+                    <td>{vehicle.plateNumber}</td>
+                  </tr>
+                  <tr>
+                    <th>Availability:</th>
+                    <td>
+                      From - {availability.pickup.toDateString()}{", "}{availability.time} <br></br> Available For - {availability.days} Days
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Deposit:</th>
+                    <td>{costs.deposit}$</td>
+                  </tr>
+                  <tr>
+                    <th>Fare Per Day:</th>
+                    <td>{costs.fare}$</td>
+                  </tr>
+                  <tr>
+                    <th>Late Return Fee:</th>
+                    <td>{costs.otherCosts}$</td>
+                  </tr>
+                  <tr>
+                    <th>Images Uploaded:</th>
+                    <td>{images.length}</td>
+                  </tr>
+                </Table>
+                <div className="image-preview">
+                  {images.map((image, index) => (
+                    <div className="image-preview-item" key={index}>
+                      <img
+                        src={URL.createObjectURL(image)}
+                        alt={`Preview ${index}`}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </form>
           </div>
         );
       default:
@@ -290,15 +327,19 @@ export const RentalCabForm = () => {
   const renderProgressBar = () => {
     const progress = ((step - 1) / 3) * 100; // Calculate progress percentage
     return (
-      <div style={{ width: "100%", height: "20px", background: "#f0f0f0" }} className="progress-bar">
-        <div
-          style={{
-            width: `${progress}%`,
-            height: "100%",
-            background: "#007bff",
-          }}
-        ></div>
-      </div>
+      // <div
+      //   style={{ width: "100%", height: "20px", background: "#f0f0f0" }}
+      //   className="progress-bar"
+      // >
+      //   <div
+      //     style={{
+      //       width: `${progress}%`,
+      //       height: "100%",
+      //       background: "#007bff",
+      //     }}
+      //   ></div>
+      // </div>
+      <ProgressBar animated now={progress} style={{ marginTop: "20px" }}/>
     );
   };
 
