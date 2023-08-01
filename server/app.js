6fc959c69fbaa91df9fa85665041f4104a451682
Gaurav Mehla <gaurav@mehla.in>
2023-08-01 13:09:43 -0700

    serving react app from server

--- server/app.js
+++ server/app.js
@@ -17,12 +17,17 @@ dotenv.config();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(cors());
 
 // Routes
 app.use(router);
 app.use('/cars', carRouter);
+
+// After Routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 /**
  * Create HTTP server.
